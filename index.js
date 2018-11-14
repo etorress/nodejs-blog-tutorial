1ef2cde9f49058f5c493949732be23641691051d
etorress <eduardotbrs@gmail.com>
2018-11-13 22:20:13 -0700

    working

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = new express();
 
 app.use(express.static('public'));
 
+var path = require("path");
+ 
 app.get('/',(req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.sendFile(__dirname + '/pages/index.html');
 });
 
 app.listen(4000, () => {
