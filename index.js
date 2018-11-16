095cc031a26f474beee9d8aeb0dddd0b0fb30ec4
etorress <eduardotbrs@gmail.com>
2018-11-15 22:40:35 -0700

    adding contacts, about and post .html

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const path = require("path");
+
 const express = require('express');
 
 const app = new express();
 
 app.use(express.static('public'));
 
-var path = require("path");
- 
 app.get('/',(req, res) =>{
-    res.sendFile(__dirname + '/pages/index.html');
+    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+});
+
+app.get('/about', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/about.html'));
+});
+
+app.get('/contact', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'/pages/contact.html'));
+});
+
+app.get('/post',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'));
 });
 
 app.listen(4000, () => {
