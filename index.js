e55687ba87183d89c947dece47b4ae1c3904be74
etorress <eduardotbrs@gmail.com>
2018-11-22 23:32:26 -0700

    now its working

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ app.use(expressEdge);
 
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
+app.get('/public', (req, res) => {
     res.render('index');
 });
 
 app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/about.html'));
+    res.sendFile(path.resolve(__dirname,'/public/about.html'));
 });
 
 app.get('/contact', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'/pages/contact.html'));
+    res.sendFile(path.resolve(__dirname,'/public/contact.html'));
 });
 
 app.get('/post',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'pages/post.html'));
+    res.sendFile(path.resolve(__dirname,'/public/post.html'));
 });
 
 app.listen(4000, () => {
