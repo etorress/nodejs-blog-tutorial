2ea8fad27b3dc57eea99264bc5d283f326ed37f4
etorress <eduardotbrs@gmail.com>
2018-11-24 19:12:59 -0700

    help

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const path = require('path');
+
 const expressEdge = require('express-edge');
+
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = new express();
+
+mongoose.connect('mongodb://localhost:27017/node-blog', {useNewUrlParser: true})
+    .then(()=> 'You are now connected to Mongo!')
+    .catch(err=> console.error('Something went wrong', err))
 
 app.use(express.static('public'));
 app.use(expressEdge);
@@ -14,15 +22,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'/public/about.html'));
+    res.sendFile(path.resolve(__dirname,'pages/about.html'));
 });
 
 app.get('/contact', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'/public/contact.html'));
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'));
 });
 
 app.get('/post',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'/public/post.html'));
+    res.sendFile(path.resolve(__dirname,'pages/post.html'));
+});
+
+app.get('/posts/new', (req, res)=>{
+    res.render('create');
 });
 
 app.listen(4000, () => {
