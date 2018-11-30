06028b4e45af740bde717fa6d58c8ef79f5e69ee
etorress <eduardotbrs@gmail.com>
2018-11-29 22:09:08 -0700

    fixed an error with users controller

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require("express-fileupload");
-const Post = require('./database/models/Post');
-
 
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
@@ -13,9 +10,8 @@ const storePostController = require('./controllers/storePost');
 const getPostController = require('./controllers/getPost');
 const createUserController = require("./controllers/createUser");
 const storeUserController = require('./controllers/storeUser');
-
 const loginController = require ("./controllers/login");
-const loginUserController = require (".controllers/loginUser");
+const loginUserController = require ("./controllers/loginUser");
 
 const app = new express();
 
@@ -26,11 +22,8 @@ mongoose.connect('mongodb://localhost:27017/node-blog', {useNewUrlParser: true})
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge);
-
-app.use(express.static('pages'));
-app.use(expressEdge);
-
 app.set('views', __dirname + '/views');
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
@@ -44,55 +37,11 @@ app.get("/", homePageController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", createPostController);
 app.post("/posts/store", storePostController);
-<<<<<<< HEAD
 app.get('/auth/login', loginController);
-app.get('/auth/loginUserController', loginUserController);
-=======
+app.post('/users/login', loginUserController);
 app.get("/auth/register", createUserController);
 app.post("/users/register", storeUserController);
->>>>>>> e5aaeaaae89a6dff2e9dddcb4d2679a6f5db03e5
 
-app.get('/', async(req, res)=>{
-    const posts = await Post.find({})
-    res.render('index',{
-        posts
-    })
-});
-
-app.get('/posts/new', (req, res)=>{
-    res.render('create');
-});
-
-app.post('/posts/store', (req, res)=>{
-    const{
-        image
-    } = req.files
-
-    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error)=>{
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`  
-        }, (error, post) =>{
-            res.redirect('/');
-        });
-    })
-});
-
-
-app.get('/about', (req, res)=>{
-    res.render('about');
-});
-
-app.get('/contact', (req, res)=>{
-    res.render('contact');
-});
-
-app.get('/post/:id', async(req, res) =>{
-    const post = await Post.findById(req.params.id)
-    res.render('post',{
-        post
-    })
-});
 
 app.listen(4000, () => {
    console.log('App listening on port 4000'); 
